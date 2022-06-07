2514c5f33cfcf88cd85dfa3f8501a3b5cfd5d13d
jinze <jinzxu@ucalgary.ca>
2022-06-06 22:12:39 -0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Jason Xu (jinzxu@ucalgary.ca)</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TouchAppIcon from '@material-ui/icons/TouchApp';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <TouchAppIcon />
+        Kudos Board
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
